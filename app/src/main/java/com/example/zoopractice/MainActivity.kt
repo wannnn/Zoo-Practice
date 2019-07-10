22c93fae76be53a3a5c1bd7d3bbb8c6eb808f701
Claire <wanru850111@gmail.com>
2019-07-10 10:46:22 +0800

    finish layout

--- app/src/main/java/com/example/zoopractice/MainActivity.kt
+++ app/src/main/java/com/example/zoopractice/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.zoopractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.zoopractice.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.statusBarColor = ContextCompat.getColor(this, R.color.colorGray)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
