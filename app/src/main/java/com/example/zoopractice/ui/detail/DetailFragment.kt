594f55f454a717670d9923099ebf5f03c49aab6f
Mao Wan Ju <wanruaiai@gmail.com>
2019-07-10 20:56:51 +0800

    edit nav_graph

--- app/src/main/java/com/example/zoopractice/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/zoopractice/ui/detail/DetailFragment.kt
@@ -1,2 +1,10 @@
 package com.example.zoopractice.ui.detail
 
+import androidx.fragment.app.Fragment
+
+class DetailFragment : Fragment()  {
+
+    companion object {
+        fun newInstance() = DetailFragment()
+    }
+}
